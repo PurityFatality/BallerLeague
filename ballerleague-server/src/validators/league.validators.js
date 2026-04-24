005482c671ff3018a5e0ca05b410663953889d08
ranwar469-tech <261976913+ranwar469-tech@users.noreply.github.com>
2026-04-24 18:27:28 +0300

    updated

--- ballerleague-server/src/validators/league.validators.js
+++
@@ -1,7 +0,0 @@
-import { body } from 'express-validator';
-
-export const createLeagueValidator = [
-  body('name').trim().notEmpty().withMessage('name is required'),
-  body('country').optional().trim(),
-  body('logo').optional().trim()
-];
--- ballerleague-server/src/validators/match.validators.js
+++
@@ -1,3 +0,0 @@
-export * from './match/common.validators.js';
-export * from './match/geocode.validators.js';
-export * from './match/query.validators.js';
